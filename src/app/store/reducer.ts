5acdfc7a15fd743c11f7d43a42b44ce188379558
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-03-13 11:13:57 +0200

    Added filtering case for the reducer

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,30 +1,40 @@
 import { FILTER_COURSES } from './actions';
 import { IAppState } from './IAppState';
 
+const courses = [
+  {
+    "id": 1,
+    "name": "Building Apps with React",
+    "topic": "ReactJS"
+  },
+  {
+    "id": 2,
+    "name": "Building Apps with Angular",
+    "topic": "AngularJS"
+  },
+  {
+    "id": 3,
+    "name": "Building Apps with Angular and Redux",
+    "topic": "Angular and Redux"
+  }
+];
+
 const initialState: IAppState = {
-  courses: [
-    {
-      "id": 1,
-      "name": "Building Apps with React",
-      "topic": "ReactJS"
-    },
-    {
-      "id": 2,
-      "name": "Building Apps with Angular",
-      "topic": "AngularJS"
-    },
-    {
-      "id": 3,
-      "name": "Building Apps with Angular and Redux",
-      "topic": "Angular and Redux"
-    }
-  ]
-};
+  courses: courses,
+  filteredCourses: courses
+}
+
+function filterCourses(state, action): IAppState {
+  return Object.assign({}, state, {
+    filteredCourses: state.courses
+      .filter(c => c.name.toLowerCase().indexOf(action.searchText.toLowerCase()))
+  });
+}
 
 export function reducer(state = initialState, action) {
   switch (action.type) {
     case FILTER_COURSES:
-      break;
+      return filterCourses(state, action);
     default:
       return state;
   }
